401c4bb0b15a60210a958992eaca17164f821f76
gdany91 <you@example.comdgarofalo52@gmail.com>
2023-01-24 17:23:16 +0100

    piu generi, non funzionante

--- index.php
+++ index.php
@@ -7,23 +7,42 @@ class Movie {
     public $director;
     public $length;
 
-    public function __construct($title, $director, $length ){
+    public array $genre;
+
+    public function __construct($title, $director, $length, array $genre){
         $this -> title = $title;
         $this -> director = $director;
         $this -> length = $length;
+        $this -> genre = $genre;
     }
     public function getMovieData(){
 
-        return 'title : ' . $this -> title .
-            '<br> directort : ' . $this -> director .
-            '<br> lenght : ' . $this -> length . '<br><br>';
+        return 'title : ' . $this->title .
+            '<br> directort : ' . $this->director .
+            '<br> lenght : ' . $this->length . '<br><br>' .
+            'genres : ' . $this -> GetMovieGenres();
 
+    }
+
+    public function GetMovieGenres(){
+        
+        $str = '';
+        foreach ( $this -> genre as $genre ) {
+
+            $str .= $genre -> GetMovieGenres() ;
+        }
+            return $str;
+
+        
     }
 }
 
-$movie1 = new Movie('Arancia meccanica', 'Stanley Kubrick', '138 min');
+$genre1 = ['dramma', 'splutter'];
 
-$movie2 = new Movie('Le iene', 'Quentin Tarantino', '130 min');
+$movie1 = new Movie('Arancia meccanica', 'Stanley Kubrick', '138 min', $genre1);
+
+$movie2 = new Movie('Le iene', 'Quentin Tarantino', '130 min', $genre1);
+
 
 echo "<h1>";
 echo $movie1 -> getMovieData();
